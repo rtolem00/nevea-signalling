6ac3b81c8b23608ed94528cd095ac7337c41c2c3
Ricardo Toledo <ricardotoledomanani@gmail.com>
2023-08-08 13:41:07 +0200

    Added some logs for debugging pourposes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { sigServer } from '@libp2p/webrtc-star-signalling-server'
 
 // Set up the server config
-const HOST = process.env.HOST || '127.0.0.1'
+const HOST = process.env.HOST || '0.0.0.0'
 const PORT = process.env.PORT || 4000
 
 async function main () {
+  console.log(`Starting server on ${HOST}:${PORT}`)
   const server = await sigServer({
     port: PORT,
     host: HOST,
@@ -12,7 +13,12 @@ async function main () {
   })
 }
 
-main()
+main().then(() => {
+  console.log('Server started successfully')
+}).catch((err) => {
+  console.error('Server failed to start:', err)
+  process.exit(1)
+})
 
 // on SIGINT signal and SIGTERM signal, stop the node
 process.on('SIGINT', async () => {
